1cdcba31bd99f1b7dbd0046b52078d2eb8bb0a78
Noob1234 <coobicroobic228@gmail.com>
2026-03-13 19:48:22 +0700

    Add blocks.js file with block type definitions and textures

---
+++ blocks.js
@@ -0,0 +1,40 @@
+const blocks = {
+    dirt: {
+        id: 1,
+        texture: 'textures/dirt.png',
+    },
+    grass: {
+        id: 2,
+        texture: 'textures/grass.png',
+    },
+    stone: {
+        id: 3,
+        texture: 'textures/stone.png',
+    },
+    wood: {
+        id: 4,
+        texture: 'textures/wood.png',
+    },
+    water: {
+        id: 5,
+        texture: 'textures/water.png',
+    },
+    sand: {
+        id: 6,
+        texture: 'textures/sand.png',
+    },
+    brick: {
+        id: 7,
+        texture: 'textures/brick.png',
+    },
+    glass: {
+        id: 8,
+        texture: 'textures/glass.png',
+    },
+    cobblestone: {
+        id: 9,
+        texture: 'textures/cobblestone.png',
+    },
+};
+
+export default blocks;
